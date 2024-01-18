c4ce9593e0ec770655b5bfa92e0ac72c40fcb175
Aizar Hafizh <aizarhafizh@gmail.com>
2024-01-18 19:42:42 +0700

    feat: add search feature based on city name

--- js/script.js
+++ js/script.js
@@ -1,98 +1,70 @@
 // API Address to current weather condition
-let currentWeatherAPI = 'https://api.weatherapi.com/v1/current.json?key=e1b28bab47bf4381b9c71958241501';
+const API = {
+  baseURL: 'https://api.weatherapi.com/v1',
+  key: 'e1b28bab47bf4381b9c71958241501',
+  current: 'current.json',
+  forecast: 'forecast.json'
+}
 
 // Parameter location
 let paramLocation = '';
 
 // Default location. Get location from user's IP Address
 if(paramLocation === ''){
-  currentWeatherAPI += '&q=auto:ip';
+  paramLocation = 'auto:ip';
 }
 
+fetchCurrentWeather();
 
-fetch(currentWeatherAPI)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('API REQUEST FAILED');
-    }
-  })
-  .then((data) => {
-    // Temperature
-    const temp = document.querySelector('.temp');
-    temp.innerText = data.current.temp_c + '\u00B0C';
+// Search event listener
+const searchBar = document.getElementById('search-loc');
+searchBar.addEventListener('keydown', (event)=>{
+  if(event.key === 'Enter'){
+    paramLocation = searchBar.value;
+    fetchCurrentWeather();
+    console.log(paramLocation);
+    event.preventDefault();
+  }  
+});
 
-    // Icon based on weather
-    const icon = document.querySelector('.temperature img');
-    icon.setAttribute('src', data.current.condition.icon);
-
-    // Location name
-    const locationDisplay = document.querySelector('.location h3');
-    locationDisplay.innerHTML = data.location.name;
-    console.log(data);
-
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+const searchButton = document.querySelector('.search-bar button');
+searchButton.addEventListener('click', (event)=>{
+  paramLocation = searchBar.value;
+  fetchCurrentWeather();
+  console.log(paramLocation);
+  event.preventDefault();
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const searchButton = document.querySelector('button');
-// searchButton.addEventListener('click', function(){
-//   const inputSearch = document.getElementById('input-loc');
-//   locationParam = inputSearch.value;
-//   console.log(locationParam);
-
-//   const locationName = document.querySelector('.location h3');
-//   locationName.innerHTML = `${locationParam}`;  
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function fetchCurrentWeather(){
+  fetch(`${API.baseURL}/${API.current}?key=${API.key}&q=${paramLocation}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('API REQUEST FAILED');
+      }
+    })
+    .then((data) => {
+      // Temperature
+      const temp = document.querySelector('.temp');
+      temp.innerText = data.current.temp_c + '\u00B0C';
+  
+      // Icon based on weather
+      const icon = document.querySelector('.temperature img');
+      icon.setAttribute('src', data.current.condition.icon);
+  
+      // Location name
+      const locationDisplay = document.querySelector('.location h3');
+      locationDisplay.innerHTML = data.location.name;
+      console.log(data);
+  
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 
 
