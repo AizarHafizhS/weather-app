3a4d160f8524cac33a8fb93bdd0ec483afdf3e7a
Aizar Hafizh <aizarhafizh@gmail.com>
2024-01-19 15:33:03 +0700

    feat: get real-time date and time but need reload page first

--- js/script.js
+++ js/script.js
@@ -3,41 +3,41 @@ const API = {
   baseURL: 'https://api.weatherapi.com/v1',
   key: 'e1b28bab47bf4381b9c71958241501',
   current: 'current.json',
-  forecast: 'forecast.json'
-}
+  forecast: 'forecast.json',
+};
 
 // Parameter location
 let paramLocation = '';
 
 // Default location. Get location from user's IP Address
-if(paramLocation === ''){
+if (paramLocation === '') {
   paramLocation = 'auto:ip';
 }
 
 fetchCurrentWeather();
 
-// Search event listener
+// Search event listener when press 'Enter'
 const searchBar = document.getElementById('search-loc');
-searchBar.addEventListener('keydown', (event)=>{
-  if(event.key === 'Enter'){
+searchBar.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
     paramLocation = searchBar.value;
     fetchCurrentWeather();
     console.log(paramLocation);
     event.preventDefault();
-  }  
+  }
 });
 
+// Search event listener when click searh button
 const searchButton = document.querySelector('.search-bar button');
-searchButton.addEventListener('click', (event)=>{
+searchButton.addEventListener('click', (event) => {
   paramLocation = searchBar.value;
   fetchCurrentWeather();
   console.log(paramLocation);
   event.preventDefault();
 });
 
-
-
-function fetchCurrentWeather(){
+// Fetch the current weather
+function fetchCurrentWeather() {
   fetch(`${API.baseURL}/${API.current}?key=${API.key}&q=${paramLocation}`)
     .then((response) => {
       if (response.ok) {
@@ -50,35 +50,75 @@ function fetchCurrentWeather(){
       // Temperature
       const temp = document.querySelector('.temp');
       temp.innerText = data.current.temp_c + '\u00B0C';
-  
+
       // Icon based on weather
       const icon = document.querySelector('.temperature img');
       icon.setAttribute('src', data.current.condition.icon);
-  
+
       // Location name
       const locationDisplay = document.querySelector('.location h3');
       locationDisplay.innerHTML = data.location.name;
       console.log(data);
-  
     })
     .catch((error) => {
       console.error(error);
     });
 }
 
+// Functions to create date and time
+const now = new Date(); // variable in order to use time n date method
+function currentDate() {
+  const date = now.getDate();
+  const dayIndex = now.getDay();
+  const daysList = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
+  const monthIndex = now.getMonth();
+  const monthList = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const year = now.getFullYear();
 
-// const now = new Date();
-// const hour = now.getHours();
-// const minute = now.getMinutes();
-// const date = now.getDate();
-// const daysList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-// const dayIndex = now.getDay();
-// const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-// const monthIndex = now.getMonth();
-// const year = now.getFullYear();
+  return `${daysList[dayIndex]}, ${date} ${monthList[monthIndex]} ${year}`;
+}
 
-// const timeSelector = document.querySelector('.time');
-// const dateSelector = document.querySelector('.date');
-// // timeSelector.innerHTML = ``
-// dateSelector.innerHTML = `${daysList[dayIndex]}, ${date} ${monthList[monthIndex]} ${year}`;
+function getCurrentTime() {
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+
+  hour %= 12;
+  hour = hour ? hour : 12; // Will return false if hour === 0
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+
+  return `${hour}:${minute}`;
+}
+
+function getAmPm() {
+  let hour = now.getHours();
+  return hour >= 12 ? 'PM' : 'AM';
+}
+
+// Date and time builder
+const timeSelector = document.querySelector('.time');
+const dateSelector = document.querySelector('.date');
+timeSelector.innerHTML = `${getCurrentTime()}<span>${getAmPm()}</span>`;
+dateSelector.innerHTML = currentDate();
